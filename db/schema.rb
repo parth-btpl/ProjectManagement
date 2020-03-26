9f934d622ae8585baf451f4bd487a926696221b8
parth-btpl <parth.patel@botreetechnologies.com>
2020-03-26 18:04:52 +0530

    Add association between project & task model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_26_121732) do
+ActiveRecord::Schema.define(version: 2020_03_26_123050) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,9 @@ ActiveRecord::Schema.define(version: 2020_03_26_121732) do
     t.datetime "delivery_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "project_id", null: false
+    t.index ["project_id"], name: "index_tasks_on_project_id"
   end
 
+  add_foreign_key "tasks", "projects"
 end
