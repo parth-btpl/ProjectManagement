07525af64f9ec1f466463bdeedebf47cd0441a47
parth-btpl <parth.patel@botreetechnologies.com>
2020-04-23 15:26:30 +0530

    Add searching on project listing page.

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,4 +4,5 @@ class Project < ApplicationRecord
   has_many :tasks, dependent: :destroy
   enum status: { start: 0, on_hold: 1, complete: 2 }
   validates :name, :description, :status, :start_date, :end_date, :internal_demo_date, presence: true
+  ransack_alias :search, :name
 end
