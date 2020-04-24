f95dbb68e11103caba1d6cbe58ff54cce1707a47
parth-btpl <parth.patel@botreetechnologies.com>
2020-04-24 15:34:00 +0530

    Add searching for task by title and description.

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,4 +4,5 @@ class Task < ApplicationRecord
   enum status: { to_do: 0, in_progress: 1, dev_done: 2, ready_for_testing: 3 }
   belongs_to :project
   validates :title, :description, :status, :delivery_date, presence: true
+  ransack_alias :search, :title_or_description
 end
