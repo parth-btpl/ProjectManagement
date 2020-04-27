f10f1e61921f4da16ee7327c0832d02c1e509d2e
parth-btpl <parth.patel@botreetechnologies.com>
2020-04-27 16:55:20 +0530

    remove user table

--- db/schema.rb
+++ db/schema.rb
@@ -66,20 +66,6 @@ ActiveRecord::Schema.define(version: 2020_04_27_074219) do
     t.index ["project_id"], name: "index_tasks_on_project_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.string "name"
-    t.integer "mobile"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "tasks"
   add_foreign_key "tasks", "projects"
